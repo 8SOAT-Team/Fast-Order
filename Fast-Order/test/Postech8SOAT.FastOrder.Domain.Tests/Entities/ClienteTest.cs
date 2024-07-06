b684c0b6e1e07289adeb389de45ad30c50f2ea2b
bessax <andrebessax@gmail.com>
2024-07-06 00:48:21 -0300

    #Update: implementação de mais alguns testes.

--- Fast-Order/test/Postech8SOAT.FastOrder.Domain.Tests/Entities/ClienteTest.cs
+++ Fast-Order/test/Postech8SOAT.FastOrder.Domain.Tests/Entities/ClienteTest.cs
@@ -1,5 +1,96 @@
-﻿namespace Postech8SOAT.FastOrder.Domain.Tests.Entities;
+﻿using Postech8SOAT.FastOrder.Domain.Entities;
+using Postech8SOAT.FastOrder.Domain.Exceptions;
+
+namespace Postech8SOAT.FastOrder.Domain.Tests.Entities;
 public class ClienteTest
-{    
+{
+    [Fact]
+    public void RetornaUmClienteValido()
+    {
+        // Arrange
+        string cpf = "12345678900";
+        string nome = "João da Silva";
+        string email = "joao@example.com";
+
+        // Act
+        var cliente = new Cliente(cpf, nome, email);
+
+        // Assert
+        Assert.Equal(cpf, cliente.Cpf);
+        Assert.Equal(nome, cliente.Nome);
+        Assert.Equal(email, cliente.Email);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void RetornaThrowsExceptionQuandoCPFeHNulo(string cpf)
+    {
+        // Arrange
+        string nome = "João da Silva";
+        string email = "joao@example.com";
+
+        // Act & Assert
+        Assert.Throws<DomainExceptionValidation>(() => new Cliente(cpf, nome, email));
+    }
+
+    [Theory]
+    [InlineData("1234567890", "Nome válido", "email@example.com")]
+    [InlineData("12345678901234567890", "Nome válido", "email@example.com")]
+    public void RetornaExcptionQuandoCPFInvalido(string cpf, string nome, string email)
+    {
+        // Act & Assert
+        Assert.Throws<DomainExceptionValidation>(() => new Cliente(cpf, nome, email));
+    }
+
+    [Theory]
+    [InlineData("12345678900", "Jo", "email@example.com")]
+    [InlineData("12345678900", "Nome válido com mais de 100 caracteres Nome válido com mais de 100 caracteres Nome válido com mais de 100 caracteres Nome válido com mais de 100 caracteres", "email@example.com")]
+    public void RetornaExceptionQuandoNomeTamanhoInvalido(string cpf, string nome, string email)
+    {
+        // Act & Assert
+        Assert.Throws<DomainExceptionValidation>(() => new Cliente(cpf, nome, email));
+    }
+
+    [Theory]
+    [InlineData("12345678900", "Nome válido", "")]
+    [InlineData("12345678900", "Nome válido", "email")]
+    public void RetornaExceptionQuandoEmailTamanhoInvalido(string cpf, string nome, string email)
+    {
+        // Act & Assert
+        Assert.Throws<DomainExceptionValidation>(() => new Cliente(cpf, nome, email));
+    }
+       
+    [Theory]
+    [InlineData("123.456.789-00", "João da Silva", "joao@example.com")]
+    [InlineData("987.654.321-09", "Maria Oliveira", "maria@example.com")]
+    public void ValidaClienteComCPFComPontuacao(string cpf, string nome, string email)
+    {
+        // Act
+        var cliente = new Cliente(cpf, nome, email);
+
+        // Assert
+        Assert.Equal("12345678900", cliente.Cpf); // O CPF deve ser armazenado sem pontuação
+        Assert.Equal(nome, cliente.Nome);
+        Assert.Equal(email, cliente.Email);
+    }
+
+    [Fact]
+    public void AtualizaClienteValido()
+    {
+        // Arrange
+        var cliente = new Cliente("12345678900", "João da Silva", "joao@example.com");
+        string novoNome = "João Silva";
+        string novoEmail = "joao.silva@example.com";
+
+        // Act
+        cliente.Update("12345678900", novoNome, novoEmail);
+
+        // Assert
+        Assert.Equal("12345678900", cliente.Cpf);
+        Assert.Equal(novoNome, cliente.Nome);
+        Assert.Equal(novoEmail, cliente.Email);
+    }
+
 
 }
---
+++ Fast-Order/test/Postech8SOAT.FastOrder.Domain.Tests/Entities/ProdutoTest.cs
@@ -0,0 +1,78 @@
+﻿using Postech8SOAT.FastOrder.Domain.Entities;
+using Postech8SOAT.FastOrder.Domain.Exceptions;
+
+namespace Postech8SOAT.FastOrder.Domain.Tests.Entities;
+public class ProdutoTest
+{
+    [Fact]
+    public void DeveRetornarExceptionQuandoNomeForNulo()
+    {
+        //Arrange
+        var nome = "";
+        var descricao = "Teste";
+        //Act
+        Action act = () => new Produto(nome, descricao, 10, "Imagem de teste");
+        //Assert
+        Assert.Throws<DomainExceptionValidation>(() => act());
+    }
+    [Fact]
+    public void DeveRetornarExceptionQuandoNomeForMenorQue3Caracteres()
+    {
+        //Arrange
+        var nome = "Te";
+        var descricao = "Teste";
+        //Act
+        Action act = () => new Produto(nome, descricao, 10, "Imagem de teste");
+        //Assert
+        Assert.Throws<DomainExceptionValidation>(() => act());
+    }
+
+    [Fact]
+    public void DeveCriarProdutoComSucesso()
+    {
+        //Arrange
+        var nome = "Teste";
+        var descricao = "Teste";
+        //Act
+        var produto = new Produto(nome, descricao, 10, "Imagem de teste");
+        //Assert
+        Assert.NotNull(produto);
+    }
+
+    [Theory]
+    [InlineData("Teste", "Teste", 10, "Imagem de teste 1")]
+    [InlineData("Teste2", "Teste2", 10, "Imagem de teste 2")]
+    [InlineData("Teste3", "Teste3", 10, "Imagem de teste 3")]
+    public void DeveCriarProdutoComSucessoParametrizado(string nome, string descricao, decimal preco, string imagem)
+    {
+        //Arrange
+        //Act
+        var produto = new Produto(nome, descricao, preco, imagem);
+        //Assert
+        Assert.NotNull(produto);
+    }
+
+    [Fact]
+    public void DeveRetornarExceptionQuandoImagemForNula()
+    {
+        //Arrange
+        var nome = "Teste";
+        var descricao = "Teste";
+        //Act
+        Action act = () => new Produto(nome, descricao, 10, "");
+        //Assert
+        Assert.Throws<DomainExceptionValidation>(() => act());
+    }
+
+    [Fact]
+    public void DeveRetornarExceptionQuandoImagemForMenorQue3Caracteres()
+    {
+        //Arrange
+        var nome = "Teste";
+        var descricao = "Teste";
+        //Act
+        Action act = () => new Produto(nome, descricao, 10, "Im");
+        //Assert
+        Assert.Throws<DomainExceptionValidation>(() => act());
+    }
+}
