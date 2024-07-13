42fec292723321bd87d2f45a5486db6711436e10
Felipe Bergmann <felipe.bergmann.f@gmail.com>
2024-07-12 23:55:47 -0300

    feat: endpoint cadastro de produtos

--- Fast-Order/src/Postech8SOAT.FastOrder.WebAPI/Endpoints/ProdutoExtensions.cs
+++ Fast-Order/src/Postech8SOAT.FastOrder.WebAPI/Endpoints/ProdutoExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Postech8SOAT.FastOrder.Domain.Entities;
-using Postech8SOAT.FastOrder.Domain.Ports.Repository;
 using Postech8SOAT.FastOrder.Domain.Ports.Service;
 using Postech8SOAT.FastOrder.WebAPI.DTOs;
 
@@ -11,21 +10,31 @@ public static class ProdutoExtensions
 {
     public static void AddEnPointProdutos(this WebApplication app)
     {
-        app.MapGet("/produto", async ([FromServices]IMapper mapper, [FromServices]IProdutoService service) =>
+        app.MapGet("/produto", async ([FromServices] IMapper mapper, [FromServices] IProdutoService service) =>
         {
-           var produtosDTO = mapper.Map<ICollection<Produto>, ICollection<ProdutoDTO>>(await service.GetAllProdutosAsync());
+            var produtosDTO = mapper.Map<ICollection<Produto>, ICollection<ProdutoDTO>>(await service.GetAllProdutosAsync());
 
             return Results.Ok(await Task.FromResult(produtosDTO));
 
         }).WithTags("Produto").WithSummary("Listagem de produtos cadastrados.").WithOpenApi();
 
-     
-        app.MapGet("/produto/{categoriaId}", async([FromServices]IMapper mapper, [FromServices]IProdutoService service, Guid categoriaId) =>
+
+        app.MapGet("/produto/categoria/{categoriaId}", async ([FromServices] IMapper mapper, [FromServices] IProdutoService service, Guid categoriaId) =>
         {
             var produtosDTO = mapper.Map<ICollection<Produto>, ICollection<ProdutoDTO>>(await service.GetProdutosByCategoria(categoriaId));
 
             return Results.Ok(await Task.FromResult(produtosDTO));
 
         }).WithTags("Produto").WithSummary("Listagem de produtos por categoria.").WithOpenApi();
+
+        app.MapPut("/produto", async ([FromServices] IMapper mapper, [FromServices] IProdutoService service, [FromBody] ProdutoDTO request) =>
+        {
+            var produto = mapper.Map<Produto>(request);
+            produto = await service.CreateProdutoAsync(produto);
+
+            var produtoDto = mapper.Map<ProdutoDTO>(produto);
+
+            return Results.Ok(produtoDto);
+        }).WithTags("Produto").WithSummary("Inclua novos produtos.").WithOpenApi();
     }
 }
