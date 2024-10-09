4437c65324299687f66871b0c67845eb651c392f
Felipe Bergmann <felipe.bergmann.f@gmail.com>
2024-10-08 21:54:13 -0300

    fix: envs

--- src/Postech8SOAT.FastOrder.WebAPI/Program.cs
+++ src/Postech8SOAT.FastOrder.WebAPI/Program.cs
@@ -12,12 +12,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-   c.AddEnumsAsStringsFilter();
+    c.AddEnumsAsStringsFilter();
 });
 
 //Registrando as dependências
 builder.Services.AddAutoMapper(typeof(Program));
-IConfiguration configuration = builder.Configuration;
+
+IConfiguration configuration = builder.Configuration.AddEnvironmentVariables().Build();
 builder.Services.ConfigureDI(configuration);
 
 builder.Services.Configure<JsonOptions>(options =>
