84fddcdcf1cad84887d034e0a1ac444c825301b4
Felipe Bergmann <felipe.bergmann.f@gmail.com>
2024-10-09 22:21:49 -0300

    chore: print connection string on error

--- src/Postech8SOAT.FastOrder.WebAPI/Services/MigracoesPendentes.cs
+++ src/Postech8SOAT.FastOrder.WebAPI/Services/MigracoesPendentes.cs
@@ -7,12 +7,17 @@ public static class MigracoesPendentes
 {
     public static void ExecuteMigration(this IApplicationBuilder app)
     {
-        using (var serviceScope = app.ApplicationServices.CreateScope())
+        using var serviceScope = app.ApplicationServices.CreateScope();
+        var serviceDb = serviceScope.ServiceProvider
+                         .GetService<FastOrderContext>();
+        try
         {
-            var serviceDb = serviceScope.ServiceProvider
-                             .GetService<FastOrderContext>();
-
             serviceDb!.Database.Migrate();
+        }
+        catch (Exception ex)
+        {
+            var conn = serviceDb!.Database.GetConnectionString();
+            throw new Exception($"connection: {conn}", ex);
         }
     }
 }
