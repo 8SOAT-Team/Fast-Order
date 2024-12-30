3d299e1dd5f058de283b4e592b9c10ca5afea3d4
bessax <andrebessax@gmail.com>
2024-12-30 18:02:50 -0300

    #Update: Criada a classe ProdutoBuilder

---
+++ test/Postech8SOAT.FastOrder.Integration.Tests/Builder/ProdutoBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Bogus;
+using Postech8SOAT.FastOrder.Domain.Entities;
+
+namespace Postech8SOAT.FastOrder.Integration.Tests.Builder;
+public class ProdutoBuilder : Faker<Produto>
+{
+    public ProdutoBuilder()
+    {
+        CustomInstantiator(f => new Produto(f.Commerce.ProductName(),f.Commerce.ProductDescription(), f.Random.Decimal(1, 1000),f.Random.String(),f.Random.Guid()));
+    }
+    public Produto Build() => Generate();
+
+}
