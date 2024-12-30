9ac72eb360f0228da3d885d513116cba6f96e214
bessax <andrebessax@gmail.com>
2024-12-30 15:02:32 -0300

    #feat: Implementação inicial do projeto de testes de integração de API

---
+++ test/Postech8SOAT.FastOrder.Integration.Tests/FastOrder_ClienteTest.cs
@@ -0,0 +1,43 @@
+﻿using Postech8SOAT.FastOrder.Controllers.Clientes.Dtos;
+using Postech8SOAT.FastOrder.Domain.Entities;
+using Postech8SOAT.FastOrder.Integration.Tests.HostTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postech8SOAT.FastOrder.Integration.Tests;
+public class FastOrder_ClienteTest:IClassFixture<FastOrderWebApplicationFactory>
+{
+    private readonly FastOrderWebApplicationFactory _factory;
+
+    public FastOrder_ClienteTest(FastOrderWebApplicationFactory factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task GET_Deve_buscar_cliente_CPF()
+    {
+        //Arrange
+        var clienteExistente = _factory.Context.Clientes.FirstOrDefault();
+        if (clienteExistente is null)
+        {
+            clienteExistente = new Cliente("185.681.710-52", "Bill Gates", "bill@email.com");
+
+            _factory.Context.Add(clienteExistente);
+            _factory.Context.SaveChanges();
+        }
+
+        var httpClient = _factory.CreateClient();
+
+        //Act
+        var response = await httpClient.GetFromJsonAsync<ClienteIdentificadoDto>($"/cliente/"+ clienteExistente.Cpf);
+
+        //Assert
+        Assert.NotNull(response);
+
+    }
+}
---
+++ test/Postech8SOAT.FastOrder.Integration.Tests/HostTest/FastOrderWebApplicationFactory.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Postech8SOAT.FastOrder.Infra.Data.Context;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Testcontainers.MsSql;
+using Polly;
+
+namespace Postech8SOAT.FastOrder.Integration.Tests.HostTest;
+public class FastOrderWebApplicationFactory: WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private IServiceScope scope;
+
+    public FastOrderContext Context { get; private set; }
+
+    private readonly MsSqlContainer _mssqlContainer = new MsSqlBuilder()
+        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")        
+        .Build();
+
+    public async Task InitializeAsync()
+    {
+
+        await _mssqlContainer.StartAsync();
+        this.scope = Services.CreateScope();
+        Context = scope.ServiceProvider.GetRequiredService<FastOrderContext>();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll(typeof(DbContextOptions<FastOrderContext>));
+
+            services.AddDbContext<FastOrderContext>(options =>
+                options
+                .UseLazyLoadingProxies()
+                .UseSqlServer(_mssqlContainer.GetConnectionString()));
+        });
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _mssqlContainer.DisposeAsync();
+    }
+
+    //public async Task<HttpClient> GetClientWithAccessTokenAsync()
+    //{
+    //    var client = this.CreateClient();
+
+    //    var user = new UserDTO { Email = "tester@email.com", Password = "Senha123@" };
+
+    //    var response = await client.PostAsJsonAsync("/auth-login", user);
+
+    //    response.EnsureSuccessStatusCode();
+
+    //    var result = await response.Content.ReadAsStringAsync();
+
+    //    var options = new JsonSerializerOptions
+    //    {
+    //        PropertyNameCaseInsensitive = true,
+    //    };
+
+    //    var token = JsonSerializer.Deserialize<UserTokenDTO>(result, options);
+
+    //    var clientAutenticado = this.CreateClient();
+
+    //    clientAutenticado.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token!.Token);
+    //    return clientAutenticado;
+    //}
+}
