554e2785f316f4d7227eb2fbffe3accf74ef900e
bessax <andrebessax@gmail.com>
2025-01-07 21:00:27 -0300

    #test: implementação de testes para o endpoint de pedidos.

---
+++ test/Postech8SOAT.FastOrder.Integration.Tests/Builder/NovoPedidoDTOInvalidoBuilder.cs
@@ -0,0 +1,25 @@
+﻿using Bogus;
+using Postech8SOAT.FastOrder.Controllers.Pedidos.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postech8SOAT.FastOrder.Integration.Tests.Builder;
+internal class NovoPedidoDTOInvalidoBuilder:Faker<NovoPedidoDTO>
+{
+    public NovoPedidoDTOInvalidoBuilder()
+    {
+        CustomInstantiator(f => new NovoPedidoDTO()
+        {
+            ClienteId = Guid.Empty,
+            ItensDoPedido = new List<NovoItemDePedido>()
+            {
+                new NovoItemDePedidoBuilder().Build(),
+                new NovoItemDePedidoBuilder().Build()
+            }
+        });
+    }
+    public NovoPedidoDTO Build() => Generate();
+}
---
+++ test/Postech8SOAT.FastOrder.Integration.Tests/Builder/RetornaIdProdutoUtil.cs
@@ -0,0 +1,14 @@
+﻿namespace Postech8SOAT.FastOrder.Integration.Tests.Builder;
+public class RetornaIdProdutoUtil
+{
+    private static List<Guid> produtosId = new List<Guid> { new Guid("0e05db30-b5ec-4e26-b79e-a43b64743ab5"),
+    new Guid("f0fdbefb-08b2-4ad7-bdfc-8c3f0e070d8e")};
+    public static Guid RetornaIdProduto()
+    {
+
+        Random random = new Random();
+        int randomIndex = random.Next(produtosId.Count);
+        Guid randomId = produtosId[randomIndex];
+        return randomId;
+    }
+}
